34423146485539816e5b05773ecb902a48796b85
Kabrax01 <rogalski.arnold@gmail.com>
2026-01-20 17:25:58 +0100

    feat: added new pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
 import { RootLayoutProps } from "@/types/general";
-import Navigation from "@/app/_components/Navigation";
-import Logo from "@/app/_components/Logo";
 import "../app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+    subsets: ["latin"],
+    display: "swap",
+});
+
 export const metadata = {
-    title: "The Wild Oasis",
+    title: {
+        template: "%s The Wild Oasis",
+        default: "Welcome / The Wild Oasis",
+    },
+    description:
+        "Luxurious cabin hotel, located in the hart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 const RootLayout = ({ children }: RootLayoutProps) => {
     return (
         <html lang="en">
-            <body className="min-h-screen bg-primary-950 text-primary-100">
-                <header>
-                    <Logo />
-                    <Navigation />
-                </header>
-                <main>{children}</main>
-                <footer>Copyright by The Wild Oasis</footer>
+            <body
+                className={`${josefin.className} antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col relative`}>
+                <Header />
+
+                <div className="flex-1 px-8 py-12 grid">
+                    <main className="max-w-7xl mx-auto w-full">{children}</main>
+                </div>
             </body>
         </html>
     );
